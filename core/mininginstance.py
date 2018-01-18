77b6b97376ca0270f551657ddb5445990d136851
Eveler <evelerwork@mail.ru>
2018-01-18 18:02:13 +1000

    Instance executable managing in progress

--- core/mininginstance.py
+++ core/mininginstance.py
@@ -4,17 +4,30 @@
 import json
 import logging
 import os
+import subprocess
 
 from datetime import datetime, timedelta
+from sys import stdout
 
 
 class Instance:
-    def __init__(self, name=""):
+    SW_HIDE = 0
+    SW_MAXIMIZE = 0
+    SW_MINIMIZE = 6
+    SW_SHOW = 5
+
+    def __init__(self, name="", wnd_state=SW_HIDE):
+        self.__process = None
         self.__stats = {}
         self.__name = name
+        self.__wnd_state = wnd_state
         stats_path = os.path.join(os.getcwd(), 'Stats')
         if not os.path.exists(stats_path):
             os.makedirs(stats_path)
+
+    @property
+    def process(self):
+        return self.__process
 
     @property
     def stats(self):
@@ -60,7 +73,6 @@ class Instance:
                     "outside fault tolerance (%s ... %s). " %
                     (name, value, tolerance_min, tolerance_max))
             else:
-                isinstance(duration, timedelta)
                 Span_Minute = min(duration.seconds * 60 / min(self.stats.get("Duration").get("TotalMinutes"), 1), 1)
                 Span_Minute_5 = min((duration.seconds * 60 / 5) / min((self.stats.get("Duration").get("TotalMinutes") / 5), 1), 1)
                 Span_Minute_10 = min((duration.seconds * 60 / 10) / min((self.stats.get("Duration").get("TotalMinutes") / 10), 1), 1)
@@ -69,3 +81,42 @@ class Instance:
                 Span_Week = min((duration.days / 7) / min((self.stats.get("Duration").get("TotalDays") / 7), 1), 1)
         except Exception as e:
             logging.error(str(e))
+
+    def start(self):
+        cmd = ""
+        cwd = ""
+        PIPE = subprocess.PIPE
+
+        si = subprocess.STARTUPINFO()
+        si.dwFlags = subprocess.STARTF_USESTDHANDLES
+        shell = False
+        if self.__wnd_state:
+            si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+            si.wShowWindow = self.__wnd_state
+            shell = True
+
+        self.__process = subprocess.Popen(
+            cmd, shell=shell, stdin=PIPE, stdout=PIPE, stderr=PIPE,
+            cwd=cwd, startupinfo=si)
+
+    def is_running(self):
+        if self.__process:
+            return self.__process.poll() is None
+        else:
+            return False
+
+    def return_code(self):
+        return self.__process.poll() if self.__process else 0
+
+    def stdout(self):
+        return self.__process.stdout if self.__process else None
+
+    def stderr(self):
+        return self.__process.stderr if self.__process else None
+
+    def stdin(self):
+        return self.__process.stdin if self.__process else None
+
+    def kill(self):
+        if self.__process:
+            self.__process.kill()
