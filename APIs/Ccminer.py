fe0489f0e5a24bacb0a7ecd7390eea37d71183c1
Eveler <evelerwork@mail.ru>
2018-03-20 18:06:04 +1000

    APIs addition

--- APIs/Ccminer.py
+++ APIs/Ccminer.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 
 # Author: Savenko Mike
+import logging
 from time import sleep
 
 from core import Plugin
@@ -14,19 +15,26 @@ class Ccminer(Plugin):
     interval = 5
 
     def get_hash_rate(self, algo, safe=False):
+        def measure(v):
+            if max(v) - min(v) >= self.delta * sum(v) / len(v):
+                return {'Average': sum(v) / len(v), 'Maximum': max(v),
+                        'Minimum': min(v), 'Count': len(v)}
+            else:
+                return {'Average': 0, 'Maximum': 0, 'Minimum': 0, 'Count': 0}
+
         hashrates = {}
         for a in algo:
-            hr = []
             while len(hashrates) < 6:
                 data = self.get_response_text()
                 data = data.split(';')
+                logging.debug(data)
                 name = self.get_algorithm(data[a])
+                hashrates[name] = []
                 value = data['KHS'] * 1000
-                hr.append({name: value})
+                hashrates[name].append(value)
                 if not safe:
                     break
                 sleep(self.interval)
+            hashrates = {k: measure(v) for k, v in hashrates.items()}
 
-        return {'Average': sum(hashrates) / len(hashrates),
-                'Maximum': max(hashrates), 'Minimum': min(hashrates),
-                'Count': len(hashrates)}
+        return hashrates
